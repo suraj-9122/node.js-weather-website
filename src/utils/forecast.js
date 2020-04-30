9f4d3572ff62ed7eb37b673d86016caa87b5da75
suraj kumar <surajkumar@surajs-MacBook-Air.local>
2020-04-30 10:01:54 +0530

    updated forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude , longitude ,callback) => {
         } else if(body.error){
             callback('Unable to find location.',undefined)
         } else {
-            callback(undefined,   body.current.weather_descriptions[0] + ' , ' + ' It is currently '  + body.current.temperature + ' degrees. Humidity : ' + body.current.humidity + ' g/m^3 moisture. Time : ' + body.current.observation_time)
+            callback(undefined,   body.current.weather_descriptions[0] + ' , ' + ' It is currently '  + body.current.temperature + ' degrees. Humidity : ' + body.current.humidity + ' g/m^3 moisture. Windspeed : ' + body.current.wind_speed + ' MPS. ' + ' Wind-direction : ' + body.current.wind_dir + '.')  
         }
     })
 }
